4db7a14dec5ff6990a9b317742a147ac2b91d4fa
ParasRawat29 <parasrawat29pr@gmail.com>
2021-08-07 13:04:27 +0530

    added react route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 function App() {
-  return <h1>fjksd</h1>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        I am home page
+      </Route>
+      <Route exact path="/starred">
+        I am starred page
+      </Route>
+      <Route>I AM 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
