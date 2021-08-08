6a9eeac059fe6b3c1b451450b0c0b219e154b682
ParasRawat29 <parasrawat29pr@gmail.com>
2021-08-08 11:59:37 +0530

    created input and fetched result from TV Maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 function Home() {
-  return <MainPageLayout>This is Home page</MainPageLayout>;
-}
+  const [input, setInput] = useState('');
 
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=hotel
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const Handleinput = e => {
+    const val = e.target.value;
+    setInput(() => val);
+  };
+
+  const HandleEnter = e => {
+    if (e.keyCode === 13) onSearch();
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={Handleinput}
+        value={input}
+        onKeyDown={HandleEnter}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+}
 export default Home;
